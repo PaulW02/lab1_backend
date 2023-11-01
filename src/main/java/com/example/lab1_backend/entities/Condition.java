5b57d3df6c5ebb304aca1218dc461419c207b030
PaulW02 <pslwi@ug.kth.se>
2023-11-01 14:07:28 +0100

    First commit

---
+++ src/main/java/com/example/lab1_backend/entities/Condition.java
@@ -0,0 +1,4 @@
+package com.example.lab1_backend.entities;
+
+public class Condition {
+}
---
+++ src/main/java/com/example/lab1_backend/entities/Patient.java
@@ -0,0 +1,51 @@
+package com.example.lab1_backend.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+
+@Entity
+@Table(name = "patients")
+public class Patient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
